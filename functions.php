7c9086ec861e5e513172dfec83910b7a89984b14
xell12686 <jlacandula@gmail.com>
2022-03-08 17:14:22 +0800

    Disable all cookies when in EU using CF_Geoplugin

--- functions.php
+++ functions.php
@@ -31,4 +31,43 @@ return $button .= "<span aria-hidden='true'></span>";
 
 }
 
+// START: Disable all cookies when in EU using CF_Geoplugin
+function prefix_footer_code() {
+    ?>
+	<script>
+        document.addEventListener("DOMContentLoaded", function(){
+            console.log('prefix_footer_code loaded...');
+        });
+    </script>
+    <?php
+}
+function unsetAllCookies() {
+    if (isset($_SERVER['HTTP_COOKIE'])) {
+        $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+        foreach($cookies as $cookie) {
+            $parts = explode('=', $cookie);
+            $name = trim($parts[0]);
+            setcookie($name, '', time()-1000);
+            setcookie($name, '', time()-1000, '/');
+        }
+    }
+}
+function remove_cookies_on_eu() {
+    if (class_exists('CF_Geoplugin') && !is_admin() ) {
+        $client_IP = do_shortcode( '[cfgeo return="ip"]' );
+        $client_continent = do_shortcode( '[cfgeo_continent_code]' );
+        echo 'client IP: ' . $client_IP;
+        echo 'client continent: ' . $client_continent;
+        if ($client_continent === 'AS' || $client_continent === 'NA') {
+            ini_set('session.use_cookies', '0');
+            unsetAllCookies();
+            echo 'client continent is in AS / NA, all cookies disabled';
+            add_action( 'wp_footer', 'prefix_footer_code' );
+        }
+    } else {
+        echo 'CF_Geoplugin NOT INSTALLED';
+    }
+}
+add_action( 'wp_loaded','remove_cookies_on_eu' );
 
+// END: Disable all cookies when in EU using CF_Geoplugin
